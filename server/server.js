2859a497389168b8d3eb8ba0d5e6b45c51ef2516
Iraton <notari.filippo@outlook.it>
2024-06-11 19:29:48 +0200

    add db fix middleware

--- server/server.js
+++ server/server.js
@@ -1,66 +1,65 @@
-let app = require('express')();
+const express = require('express');
+let app = express();
 let http = require('http').Server(app);
-const multer = require("multer");
 let mysql = require('mysql');
+const bodyParser = require('body-parser');
 let db = require('./db.js'); //dati database in un'altro file
 let con;
 
 //mi connetto al database
 con = mysql.createConnection({
-    host: db.host,
-    user: db.user,
-    password: db.password,
-    database: db.database
+  host: db.host,
+  user: db.user,
+  password: db.password,
+  database: db.database
 });
 
 con.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to the database!");
+  if (err) throw err;
+  console.log("Connected to the database!");
 });
 
-//Create new tutorial
-// for parsing multipart/form-data
-app.use(multer().any());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  console.log('Username:', username);
+  console.log('Password:', password);
 
-    let username = req.body.username;
-    let password = req.body.password;
-    console.log('Username:', username);
-    console.log('Password:', password);
+  res.cookie('username', username);
 
-    res.cookie('username', username);
+  //controllo sql se è un venditore o un cliente
 
-    //controllo sql se è un venditore o un cliente
+  res.cookie('tipo', 'cliente');
+  res.cookie('tipo', 'vendire');
 
-    res.cookie('tipo', 'cliente');
-    res.cookie('tipo', 'vendire');
+  /*
+      con.query("INSERT INTO `tutorial` (`Titolo`, `Descrizione`) VALUES (?,?)", [titolo, descrizione], function (err) {
+          if (err) throw err;
+          return res.status(201).send(`
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <script type="text/javascript">
+                setTimeout(function() {
+                  window.location.href = 'http://localhost/upload/';
+                }, 2000);
+              </script>
+            </head>
+            <body>
+            <h2>Tutorial created successfully</h2>
+              <p>Redirecting in 2 seconds...</p>
+            </body>
+          </html>
+        `);
+  
+        
+      });*/
 
-/*
-    con.query("INSERT INTO `tutorial` (`Titolo`, `Descrizione`) VALUES (?,?)", [titolo, descrizione], function (err) {
-        if (err) throw err;
-        return res.status(201).send(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <script type="text/javascript">
-              setTimeout(function() {
-                window.location.href = 'http://localhost/upload/';
-              }, 2000);
-            </script>
-          </head>
-          <body>
-          <h2>Tutorial created successfully</h2>
-            <p>Redirecting in 2 seconds...</p>
-          </body>
-        </html>
-      `);
-
-      
-    });*/
-
-    res.redirect('http://localhost/auto.html');
-    //console.log('body:', req.body);
+  res.redirect('http://localhost/auto.html');
+  console.log('body:', req.body);
 
 
 });
@@ -69,15 +68,15 @@ app.post('/login', (req, res) => {
 //Upload file
 
 app.post('/signin', (req, res) => {
-    let username = req.body.username;
-    let password = req.body.password;
-    let venditorecliente = req.body.venditorecliente;
-    console.log('Username:', username);
-    console.log('Password:', password);
-    console.log('venditorecliente:', venditorecliente);
+  let username = req.body.username;
+  let password = req.body.password;
+  let venditorecliente = req.body.venditorecliente;
+  console.log('Username:', username);
+  console.log('Password:', password);
+  console.log('venditorecliente:', venditorecliente);
 });
 
 
 http.listen(3000, function () {
-    console.log('listening on *:3000');
+  console.log('listening on *:3000');
 });
